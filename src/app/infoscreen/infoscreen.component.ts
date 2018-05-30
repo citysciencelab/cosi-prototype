4fbbaa199e6bffd7f19d7f3f31684b55dd43ca92
Till Degkwitz <till.degkwitz@hcu-hamburg.de>
2018-05-30 17:37:06 +0200

    Infroscree changes on click sometimes (WIP)

--- src/app/infoscreen/infoscreen.component.ts
+++ src/app/infoscreen/infoscreen.component.ts
@@ -15,15 +15,24 @@ export class InfoscreenComponent implements OnInit {
   kita: Kita;
   statisticalArea: StatisticalArea;
 
-  private _url = 'assets/data/grobo-data.json';
-  private rawData;
+  private _urlDistrictProfiles = 'assets/data/grobo-data.json';
+  private _urlInfrastructure = 'assets/data/infrastructure-data.json';
+
+  // private rawData;
   // Charts
   public pieData;
-  public pieData2;
+  public pieTitle: string;
+
+  public columnData2;
+  public column2Title: string;
+
   public lineData;
   public lineCategories;
+  public lineTitle: string;
+
   public columnData;
   public columnCategories;
+  public columnTitle: string;
 
 
   constructor(private localStorageService: LocalStorageService,
@@ -34,16 +43,7 @@ export class InfoscreenComponent implements OnInit {
 
   ngOnInit(): void {
     this.localStorageService.registerMessageCallback(this.receiveMessage.bind(this));
-
-    this.getParticipationData().subscribe(
-      data => {
-        const testData = <Object []> data;
-        this.processChartData(testData);
-      },
-      error => {
-        console.log(error);
-      }
-    );
+    this.calculateInitialData();
   }
 
   receiveMessage(message: LocalStorageMessage<Kita | StatisticalArea>) {
@@ -57,14 +57,18 @@ export class InfoscreenComponent implements OnInit {
           this.statisticalArea = <StatisticalArea>message.data;
           break;
       }
+    } else if (message.type === 'deselect') {
+      console.log(message)
     } else if (message.type === 'topic-select') {
-      // TODO: hier die Neuerstellung der Charts anstossen
       switch (message.data.name) {
         case 'nahversorgung':
+          this.calculateInfrastructureData();
           break;
         case 'kitas':
+          this.calculateInitialData();
           break;
         case 'grünflächen':
+          this.calculateInitialData();
           break;
       }
     }
@@ -77,48 +81,90 @@ export class InfoscreenComponent implements OnInit {
 
 
   /*
-*   Recalculates the dataproviders (charts, tables etc.) with the changed data
-*/
+   *   Recalculates the dataproviders (charts, tables etc.) with the changed data
+  */
 
-  private processChartData(data: Object[]) {
-    this.rawData = data;
+  calculateInitialData() {
+    this.getJSONData(this._urlDistrictProfiles).subscribe(
+      data => {
+        const jsonData = <Object []> data;
+        this.zone.run(() => {
 
-    this.zone.run(() => {
+          // // Always render categories first!
+          this.lineCategories = this.chartUtils.getUniqueSeriesNames(jsonData, ['jahr']);
+          this.lineData = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'Geburten', 'jahr', this.lineCategories, 'Groß Borstel');
+          this.lineTitle = 'Geburten der umliegenden Stadtteile';
 
-      // // Always render categories first!
-      this.lineCategories = this.chartUtils.getUniqueSeriesNames(this.rawData, ['jahr']);
-      this.lineData = this.chartUtils.getSeriesData(this.rawData, 'Stadtgebiet',
-        'Geburten', 'jahr', this.lineCategories, 'Groß Borstel');
+          // // Always render categories first!
+          this.columnCategories = this.chartUtils.getUniqueSeriesNames(jsonData, ['Stadtgebiet']);
+          this.columnData = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'Anteil_der_unter_18_J_hrigen_in', 'jahr', ['2016'], 'Groß Borstel');
+          this.columnTitle = 'Anteil der Bevölkerung < 18 Jahren (2016)';
 
-      // // Always render categories first!
-      this.columnCategories = this.chartUtils.getUniqueSeriesNames(this.rawData, ['Stadtgebiet']);
-      this.columnData = this.chartUtils.getSeriesData(this.rawData, 'Stadtgebiet',
-        'Anteil_der_unter_18_J_hrigen_in', 'jahr', ['2016'], 'Groß Borstel');
+          this.pieTitle = 'Bevölkerungsanteile';
+          this.pieData = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'Bev_lkerung', 'jahr', ['2016'], 'Groß Borstel');
 
-      this.pieData = {};
-      this.pieData['data'] = [{
-        name: 'Bevölkerung',
-        y:  8459
-      }, {
-        name: 'Zuzüge',
-        y: 1458
-      }, {
-        name: 'Fortzüge',
-        y: 1770
-      }];
+          this.columnData2 = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'Anteil_der_Bev_lkerung_mit_Migrations_hintergrund_in', 'jahr', ['2016'], 'Groß Borstel');
+          this.column2Title = 'Migrationshintergrund in % der Stadtteile (2016)';
 
-      this.pieData2 = this.chartUtils.getSeriesData(this.rawData, 'Stadtgebiet',
-        'Anteil_der_Bev_lkerung_mit_Migrations_hintergrund_in', 'jahr', ['2016'], 'Groß Borstel');
-      //
-
-
-      // this.lineCategories = this.chartUtils.getUniqueSeriesNames(this.rawData, ['jahr']);
-      // this.lineData = this.chartUtils.getSumData(this.rawData,['jahr'], ['Geburten']);
-    });
+          // this.lineCategories = this.chartUtils.getUniqueSeriesNames(this.jsonData, ['jahr']);
+          // this.lineData = this.chartUtils.getSumData(this.jsonData,['jahr'], ['Geburten']);
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
-  getParticipationData() {
-    return this._http.get(this._url);
+  calculateInfrastructureData() {
+    this.getJSONData(this._urlInfrastructure).subscribe(
+      data => {
+        const jsonData = <Object []> data;
+        this.zone.run(() => {
+
+          // // Always render categories first!
+          // this.lineCategories = this.chartUtils.getUniqueSeriesNames(jsonData, ['jahr']);
+          // this.lineData = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+          //   'Geburten', 'jahr', this.lineCategories, 'Groß Borstel');
+
+          // // Always render categories first!
+          this.columnCategories = this.chartUtils.getUniqueSeriesNames(jsonData, ['Stadtgebiet']);
+          this.columnData = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'apotheken', 'jahr', ['2016'], 'Groß Borstel');
+          this.columnTitle = 'Apotheken der Stadtteile';
+
+          // this.pieData = {};
+          // this.pieData['data'] = [{
+          //   name: 'Bevölkerung',
+          //   y:  8459
+          // }, {
+          //   name: 'Zuzüge',
+          //   y: 1458
+          // }, {
+          //   name: 'Fortzüge',
+          //   y: 1770
+          // }];
+
+          this.columnData2 = this.chartUtils.getSeriesData(jsonData, 'Stadtgebiet',
+            'supermaerkte', 'jahr', ['2016'], 'Groß Borstel');
+          this.column2Title = 'Supermärkte der Stadtteile'
+
+          // this.lineCategories = this.chartUtils.getUniqueSeriesNames(this.jsonData, ['jahr']);
+          // this.lineData = this.chartUtils.getSumData(this.jsonData,['jahr'], ['Geburten']);
+        });
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getJSONData(url: string) {
+    return this._http.get(url);
   }
 
 }
